b6a9689ee11ed5a143720aa7adbc3ed9590e37e4
Farhan Ahmed <farhan1086@gmail.com>
2019-10-27 12:30:07 +0530

    Added data table script and excel file

---
+++ ImportExcelFileDataAngularJS_Demo/App_Data/Table script.sql
@@ -0,0 +1,24 @@
+USE [MvcDemo]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Employee](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](50) NULL,
+	[Position] [nvarchar](50) NULL,
+	[Office] [nvarchar](50) NULL,
+	[Age] [int] NULL,
+	[Salary] [money] NULL,
+ CONSTRAINT [PK_Employee] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
