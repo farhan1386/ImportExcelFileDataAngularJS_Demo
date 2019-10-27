4fecf3e5101493b0fb7144067f0f3ba08ba6382c
Farhan Ahmed <farhan1086@gmail.com>
2019-10-27 11:59:59 +0530

    Added Model, controller and view

---
+++ ImportExcelFileDataAngularJS_Demo/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using ImportExcelFileDataAngularJS_Demo.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace ImportExcelFileDataAngularJS_Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetEmployees()
+        {
+            using (EmployeeContext db = new EmployeeContext())
+            {
+                List<Employee> employees = db.Employees.ToList();
+                return Json(new { data = employees }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        public ActionResult ImportData()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ImportData(List<Employee> employees)
+        {
+            bool status = false;
+            if (ModelState.IsValid)
+            {
+                using (EmployeeContext db = new EmployeeContext())
+                {
+                    foreach (var i in employees)
+                    {
+                        db.Employees.Add(i);
+                    }
+                    db.SaveChanges();
+                    status = true;
+                }
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+    }
+}
---
+++ ImportExcelFileDataAngularJS_Demo/Scripts/ImportData.js
@@ -0,0 +1,56 @@
+﻿var app = angular.module('MyApp', []);
+app.controller('MyController', ['$scope', '$http', function ($scope, $http) {
+    $scope.SelectedFileForUpload = null;
+    $scope.UploadFile = function (files) {
+        $scope.$apply(function () { //I have used $scope.$apply because I will call this function from File input type control which is not supported 2 way binding
+            $scope.Message = "";
+            $scope.SelectedFileForUpload = files[0];
+        })
+    }
+    //Parse Excel Data 
+    $scope.ParseExcelDataAndSave = function () {
+        var file = $scope.SelectedFileForUpload;
+        if (file) {
+            var reader = new FileReader();
+            reader.onload = function (e) {
+                var data = e.target.result;
+                //XLSX from js-xlsx library , which I will add in page view page
+                var workbook = XLSX.read(data, { type: 'binary' });
+                var sheetName = workbook.SheetNames[0];
+                var excelData = XLSX.utils.sheet_to_row_object_array(workbook.Sheets[sheetName]);
+                if (excelData.length > 0) {
+                    //Save data 
+                    $scope.SaveData(excelData);
+                }
+                else {
+                    $scope.Message = "No data found";
+                }
+            }
+            reader.onerror = function (ex) {
+                console.log(ex);
+            }
+
+            reader.readAsBinaryString(file);
+        }
+    }
+    // Save excel data to our database
+    $scope.SaveData = function (excelData) {
+        $http({
+            method: "POST",
+            url: "/Home/ImportData",
+            data: JSON.stringify(excelData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(function (data) {
+            if (data.status) {
+                $scope.Message = excelData.length + " record inserted";
+            }
+            else {
+                $scope.Message = "Failed";
+            }
+        }, function (error) {
+            $scope.Message = "Error";
+        })
+    }
+}])
